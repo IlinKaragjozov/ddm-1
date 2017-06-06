c10e0da8571afc4c1ba731dddf164d605523144b
IlinKaragjozov <ilinfizika@gmail.com>
2017-06-06 23:39:08 +0100

    init/init_geant4_plus_ROOT.sh

---
+++ OpNovice/CMakeFiles/DDM.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/vagrant/DDM/DDM.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/DDM.cc.o"
+  "/vagrant/DDM/src/DDMActionInitialization.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMActionInitialization.cc.o"
+  "/vagrant/DDM/src/DDMDetectorConstruction.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMDetectorConstruction.cc.o"
+  "/vagrant/DDM/src/DDMPhysicsList.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMPhysicsList.cc.o"
+  "/vagrant/DDM/src/DDMPhysicsListMessenger.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMPhysicsListMessenger.cc.o"
+  "/vagrant/DDM/src/DDMPrimaryGeneratorAction.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMPrimaryGeneratorAction.cc.o"
+  "/vagrant/DDM/src/DDMPrimaryGeneratorMessenger.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMPrimaryGeneratorMessenger.cc.o"
+  "/vagrant/DDM/src/DDMRootManager.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMRootManager.cc.o"
+  "/vagrant/DDM/src/DDMRunAction.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMRunAction.cc.o"
+  "/vagrant/DDM/src/DDMStackingAction.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMStackingAction.cc.o"
+  "/vagrant/DDM/src/DDMSteppingAction.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMSteppingAction.cc.o"
+  "/vagrant/DDM/src/DDMSteppingVerbose.cc" "/vagrant/OpNovice/CMakeFiles/DDM.dir/src/DDMSteppingVerbose.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4GEOM_USE_USOLIDS"
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_88/x86_64-slc6-gcc49-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_88/x86_64-slc6-gcc49-opt/include/Geant4"
+  "/cvmfs/sft.cern.ch/lcg/releases/XercesC/3.1.3-05808/x86_64-slc6-gcc49-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/VecGeom/v0.3.rc-43240/x86_64-slc6-gcc49-opt/lib/CMake/USolids/../../../include"
+  "/cvmfs/sft.cern.ch/lcg/releases/qt/4.8.7-0b84e/x86_64-slc6-gcc49-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/qt/4.8.7-0b84e/x86_64-slc6-gcc49-opt/include/QtCore"
+  "/cvmfs/sft.cern.ch/lcg/releases/qt/4.8.7-0b84e/x86_64-slc6-gcc49-opt/include/QtGui"
+  "/cvmfs/sft.cern.ch/lcg/releases/qt/4.8.7-0b84e/x86_64-slc6-gcc49-opt/include/QtOpenGL"
+  "/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.08.06-c8fb4/x86_64-slc6-gcc49-opt/include"
+  "/vagrant/DDM/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpNovice/CMakeFiles/DDM.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DDM.dir/DDM.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMActionInitialization.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMDetectorConstruction.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMPhysicsList.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMPhysicsListMessenger.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMPrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMRootManager.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMRunAction.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMStackingAction.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMSteppingAction.cc.o"
+  "CMakeFiles/DDM.dir/src/DDMSteppingVerbose.cc.o"
+  "DDM.pdb"
+  "DDM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DDM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
